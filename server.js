57ce31aff5f9fca4d8a859bcb0500fdf9b90a473
Jiacheng Hou <hou.jiacheng.w@gmail.com>
2020-06-02 01:05:57 -0700

    Add prompt for username

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('public'))
 
 app.get('/', function(req, res){
     res.sendFile(`${__dirname}/public/index.html`)
-})
+});
 
 let server = app.listen(PORT, function(){
     console.log('Server started!!');
@@ -17,19 +17,31 @@ let server = app.listen(PORT, function(){
 
 let io = socket(server);
 
+users = [];
 io.on('connection', function(socket){
     console.log('Host Id Connected: ', socket.id);
     let tempId = socket.id;
 
-    socket.on('sent username', (usr) => {
-        io.emit('sent username', usr)
+
+
+    socket.on('set username', function(usr) {
+        console.log(usr);
         console.log(tempId + ' username: ' + usr);
+
+        if(users.indexOf(usr) > -1) {
+            socket.emit('userExists', usr + ' username is taken! Try some other username.');
+         } else {
+            users.push(usr);
+            socket.emit('userSet', {username: usr});
+         }
     })
+
 
     socket.on('disconnect', (e) => {
         console.log('Host Id Disconnected: ', tempId);
     })
 });
+
 
 io.on('connection', function(socket) {
     socket.on('sent message', (msg) => {
@@ -37,3 +49,4 @@ io.on('connection', function(socket) {
         console.log('message: ' + msg);
     })
 })
+
